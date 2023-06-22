848866ccf6cfc33fa7a7822d0e074e14a67d30a5
mj <malika.housni@gmail.com>
2023-06-22 16:38:26 +0200

    callback

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,21 @@
 class Reservation < ApplicationRecord
   belongs_to :listing
-  has_one :mission, -> { where(mission_type: 'checkout_checkin') }, dependent: :destroy
-  has_one :last_checkout_mission, class_name: 'Mission', dependent: :destroy
+  has_one :first_checkin_mission, class_name: 'Mission', dependent: :destroy
   has_one :checkout_checkin_mission, class_name: 'Mission', dependent: :destroy
+
+  after_create :create_checkout_checkin_mission
+
+  private
+
+  def create_checkout_checkin_mission
+    return if listing.last_checkout_mission&.date == end_date
+
+    # Create the checkout_checkin mission
+    Mission.create!(
+      listing: listing,
+      mission_type: 'checkout_checkin',
+      date: end_date,
+      price: listing.num_rooms * 10
+    )
+  end
 end
